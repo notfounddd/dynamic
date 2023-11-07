d07e270a7bd61c73a35845286ffbaed4593b7e39
not_founddddddddddddddddddd <128001277+notfounddd@users.noreply.github.com>
2023-11-07 21:35:59 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,72 +1,87 @@
 #include <iostream>
 
+template<typename T>
 class DynamicArray {
 private:
-    int *m_array;
+    T *m_array;
     int m_size;
 
 public:
     // Конструктор с заполнением одинаковыми элементами
-    DynamicArray(int size, int value) {
+    DynamicArray(int size, T value) {
         m_size = size;
-        m_array = new int[m_size];
+        m_array = new T[m_size];
         for (int i = 0; i < m_size; i++) {
             m_array[i] = value;
         }
     }
 
+    // Конструктор копирования
+    DynamicArray(const DynamicArray &other) {
+        m_size = other.m_size;
+        m_array = new T[m_size];
+        for (int i = 0; i < m_size; i++) {
+            m_array[i] = other.m_array[i];
+        }
+    }
+
     // Деструктор
     ~DynamicArray() {
-        for (size_t i = 0; i < m_size; i++) {
-            m_array[i] = 0;
-        }
         delete[] m_array;
         m_array = nullptr;
         std::cout << "Destructor" << std::endl;
     }
 
+    // Оператор присваивания
+    DynamicArray &operator=(const DynamicArray &other) {
+        if (this != &other) {
+            delete[] m_array;
+            m_size = other.m_size;
+            m_array = new T[m_size];
+            for (int i = 0; i < m_size; i++) {
+                m_array[i] = other.m_array[i];
+            }
+        }
+        return *this;
+    }
+
     // Метод для увеличения размера массива
-    void increaseSize(int newSize, int value) {
+    void increaseSize(int newSize, T value) {
         if (newSize <= m_size) {
-            std::cout << "Erorr : New size < = current size" << std::endl;
+            std::cout << "Error: New size <= current size" << std::endl;
             return;
         }
-
-        int* newArray = new int[newSize];
+        T *newArray = new T[newSize];
         for (int i = 0; i < m_size; i++) {
             newArray[i] = m_array[i];
         }
-
         for (int i = m_size; i < newSize; i++) {
             newArray[i] = value;
         }
-
         delete[] m_array;
         m_array = newArray;
         m_size = newSize;
     }
-
 
     // Метод для уменьшения размера массива
     void decreaseSize(int newSize) {
         if (newSize >= m_size) {
-            std::cout << "Erorr : New size > = current size" << std::endl;
+            std::cout << "Error: New size >= current size" << std::endl;
             return;
         }
-
-        int *newArray = new int[newSize];
+        T *newArray = new T[newSize];
         for (int i = 0; i < newSize; i++) {
             newArray[i] = m_array[i];
         }
-
         delete[] m_array;
         m_array = newArray;
         m_size = newSize;
     }
-    //Вывод массив
+
+    // Вывод массива
     void printArray() {
         for (int i = 0; i < m_size; i++) {
-            std::cout << m_array[i] << " ";
+            std::cout << m_array[i] << ' ';
         }
         std::cout << std::endl;
     }
@@ -74,14 +89,40 @@ public:
 
 int main() {
 
-    DynamicArray myArray(5, 10);
-    myArray.printArray();
 
-    myArray.increaseSize(8,5);
-    myArray.printArray();
+    //2 Вида массивов
+    DynamicArray<int> intArray(5, 10);
+    intArray.printArray();
+    intArray.increaseSize(8, 5);
+    intArray.printArray();
+    intArray.decreaseSize(6);
+    intArray.printArray();
 
-    myArray.decreaseSize(6);
-    myArray.printArray();
-    
+    DynamicArray<double> doubleArray(5, 3.14);
+    doubleArray.printArray();
+    doubleArray.increaseSize(8, 2.72);
+    doubleArray.printArray();
+    doubleArray.decreaseSize(6);
+    doubleArray.printArray();
+
+
+    //2 конструктора копирования
+    DynamicArray<int> NewArrayInt(intArray);
+    NewArrayInt.printArray();
+
+    DynamicArray<double> NewArrayDouble(doubleArray);
+    NewArrayDouble.printArray();
+
+
+    //2 оператора присваивания
+    DynamicArray<int> IntArray(3, 7);
+    IntArray.printArray();
+    IntArray = intArray;
+    IntArray.printArray();
+
+    DynamicArray<double> DoubleArray(10, 0.0001);
+    DoubleArray.printArray();
+    DoubleArray = doubleArray;
+    DoubleArray.printArray();
     return 0;
 }
